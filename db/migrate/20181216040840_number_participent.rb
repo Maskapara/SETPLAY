895de3bca3d86879e934dc75243a3978f274445c
vagrant <vagrant@localhost.localdomain>
2018-12-16 10:17:20 +0000

    migration file bugfix

--- db/migrate/20181216040840_number_participent.rb
+++ db/migrate/20181216040840_number_participent.rb
@@ -1,5 +1,5 @@
 class NumberParticipent < ActiveRecord::Migration[5.2]
   def change
-    add_column :posts, :number_participent, :intger
+    add_column :posts, :number_participent, :integer
   end
 end
