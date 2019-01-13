bd69d999b202c707adcded43b335a888d401b338
vagrant <vagrant@localhost.localdomain>
2019-01-13 02:39:08 +0000

    function create account

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
+
+  before_action :authenticate_user,{only: [:create]}
+  before_action :forbid_login_user,{only: [:A_create,:login]}
+
   def top
     @post = Post.new
   end
@@ -12,6 +16,7 @@ class HomeController < ApplicationController
   end
 
   def create
+
     @post = Post.new(orgnizer: params[:orgnizer], contact: params[:contact], content: params[:content], game_title: params[:game_title])
     if @post.save
       flash[:notice] = "大会ページを生成しました"
@@ -25,12 +30,11 @@ class HomeController < ApplicationController
   def login
     @user = User.find_by(email: params[:email])
     
-    # ログインしている場合
     if @user && @user.authenticate(params[:password])
       @error_message = nil
       session[:user_id] = @user.id
       flash[:notice] = "ログインしました"
-      redirect_to("/home/plan")
+      redirect_to("/home/top")
     else
       @error_message = "メールアドレスまたはパスワードが間違っています"
       @email = params[:email]
@@ -46,9 +50,26 @@ class HomeController < ApplicationController
   end
 
   def A_create
-    @user = User.new(name: params[:name],
+    @check = User.find_by(name: params[:name],
+                          email: params[:email])
+
+    if @check
+      flash[:notice] = "他アカウントの登録情報と重複する為に作成出来ませんでした。"
+      @name = params[:name]
+      @email = params[:email]
+      @password = params[:password]
+      redirect_to("/account")
+    else
+      flash[:notice] = "アカウントを新規作成しました"
+      @user = User.new(name: params[:name],
                      email: params[:email],
                      password: params[:password])
+
+      @user.save
+      session[:user_id] = @user.id
+      redirect_to("/home/top")
+    end
+
   end
 
 end
